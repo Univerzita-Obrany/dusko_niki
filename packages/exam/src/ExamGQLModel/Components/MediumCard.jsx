7eea760825ecad82567b0f266f520008dcad09c6
Jan Barton <jan.barton@unob.cz>
2026-05-31 19:22:56 +0200

    test

--- packages/exam/src/ExamGQLModel/Components/MediumCard.jsx
+++ packages/exam/src/ExamGQLModel/Components/MediumCard.jsx
@@ -32,6 +32,7 @@ export const MediumCard = ({ item, children }) => {
     return (
         <CardCapsule title={<><PersonFill /> <Link item={item} /></>}>
             {children}
+            test
             <MediumContent item={item}>
             </MediumContent>
         </CardCapsule>
